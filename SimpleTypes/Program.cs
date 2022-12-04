f158b10c96caa089e86003457206de7c8a303d57
volodie <119340399+volodie@users.noreply.github.com>
2022-12-04 11:49:03 +0300

    Lab1_VladimirNatyazhenko

---
+++ SimpleTypes/Program.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bynary operators
+
+            // operator +
+
+            //byte number1 = 2;
+            //byte number2 = 3;
+            ////byte number3 = number1 + number2; this operator not compatible for type byte
+            //byte number3 = (byte)(number1 + number2);
+
+            //sbyte number1 = -4;
+            //sbyte number2 = -66;
+            //sbyte number3 = number1 + number2; this operator not compatible for type sbyte
+            //sbyte number3 = (sbyte)(number1 + number2);
+
+            //short number1 = -4;
+            //short number2 = 10066;
+            ////short number3 = number1 + number2; this operator not compatible for type short
+            //short number3 = (short)(number1 + number2);
+
+            //ushort number1 = 4;
+            //ushort number2 = 10066;
+            //ushort number3 = number1 + number2; this operator not compatible for type ushort
+            //ushort number3 = (ushort)(number1 + number2);
+
+            //int number1 = 4;
+            //int number2 = 20000;
+            //int number3 = number1 + number2;
+
+            //uint number1 = 4;
+            //uint number2 = 20000;
+            //uint number3 = number1 + number2;
+
+            //long number1 = 4;
+            //long number2 = -20000;
+            //long number3 = number1 + number2;
+
+            //ulong number1 = 4;
+            //ulong number2 = 20000;
+            //ulong number3 = number1 + number2;
+
+            //double number1 = 4;
+            //double number2 = 20000;
+            //double number3 = number1 + number2;
+
+            //float number1 = 4;
+            //float number2 = 20000;
+            //float number3 = number1 + number2;
+
+            //char symbol1 = '1';
+            //char symbol2 = '2';
+            ////char symbol3 = symbol1 + symbol2;  this operator not compatible for type char
+            //char symbol3 = (char)(symbol1 + symbol2);
+            //Console.WriteLine(symbol3);
+            //Console.ReadKey();
+
+            //bool logic1 = true;
+            //bool logic2 = false;
+            //bool logic3 = logic1 + logic2; this operator not compatible for type bool
+
+            // operator -
+
+            //byte number1 = 77;
+            //byte number2 = 3;
+            //byte number3 = number1 - number2; this operator not compatible for type byte
+            //byte number3 = (byte)(number1 - number2);
+
+            //sbyte number1 = -4;
+            //sbyte number2 = -66;
+            ////sbyte number3 = number1 - number2; this operator not compatible for type sbyte
+            //sbyte number3 = (sbyte)(number1 - number2);
+
+            //short number1 = -4;
+            //short number2 = 10066;
+            //short number3 = number1 - number2; this operator not compatible for type short
+            //short number3 = (short)(number1 - number2);
+
+            //ushort number1 = 4000;
+            //ushort number2 = 6;
+            ////ushort number3 = number1 - number2; this operator not compatible for type ushort
+            //ushort number3 = (ushort)(number1 - number2);
+
+            //int number1 = 4;
+            //int number2 = 20000;
+            //int number3 = number1 - number2;
+
+            //uint number1 = 800;
+            //uint number2 = 200;
+            //uint number3 = number1 - number2;
+
+            //long number1 = 4;
+            //long number2 = -20000;
+            //long number3 = number1 - number2;
+
+            //ulong number1 = 40000;
+            //ulong number2 = 20000;
+            //ulong number3 = number1 - number2;
+
+            //double number1 = 4;
+            //double number2 = 20000;
+            //double number3 = number1 - number2;
+
+            //float number1 = 4;
+            //float number2 = 20000;
+            //float number3 = number1 - number2;
+
+            //char symbol1 = '1';
+            //char symbol2 = '2';
+            ////char symbol3 = symbol1 - symbol2;  this operator not compatible for type char
+            //char symbol3 = (char)(symbol1 - symbol2);
+            //Console.WriteLine(symbol3);
+            //Console.ReadKey();
+
+            //bool logic1 = true;
+            //bool logic2 = false;
+            //bool logic3 = logic1 - logic2; this operator not compatible for type bool
+
+
+            // operator /
+
+            //byte number1 = 77;
+            //byte number2 = 3;
+            ////byte number3 = number1 / number2; this operator not compatible for type byte
+            //byte number3 = (byte)(number1 / number2);
+
+            //sbyte number1 = -4;
+            //sbyte number2 = -66;
+            ////sbyte number3 = number1 / number2; this operator not compatible for type sbyte
+            //sbyte number3 = (sbyte)(number1 / number2);
+
+            //short number1 = -4;
+            //short number2 = 10066;
+            //short number3 = number1 / number2; this operator not compatible for type short
+            //short number3 = (short)(number1 / number2);
+
+            //ushort number1 = 4000;
+            //ushort number2 = 6;
+            ////ushort number3 = number1 / number2; this operator not compatible for type ushort
+            //ushort number3 = (ushort)(number1 / number2);
+
+            //int number1 = 40000;
+            //int number2 = 20000;
+            //int number3 = number1 / number2;
+
+            //uint number1 = 800;
+            //uint number2 = 200;
+            //uint number3 = number1 / number2;
+
+            //long number1 = 4;
+            //long number2 = -20000;
+            //long number3 = number1 / number2;
+
+            //ulong number1 = 40000;
+            //ulong number2 = 20000;
+            //ulong number3 = number1 / number2;
+
+            //double number1 = 4;
+            //double number2 = 20000;
+            //double number3 = number1 / number2;
+
+            //float number1 = 4;
+            //float number2 = 20000;
+            //float number3 = number1 / number2;
+
+            //char symbol1 = '1';
+            //char symbol2 = '2';
+            ////char symbol3 = symbol1 / symbol2;  this operator not compatible for type char
+            //char symbol3 = (char)(symbol1 / symbol2);
+            //Console.WriteLine(symbol3); //notrhing writes on a console
+            //Console.ReadKey();
+
+            //bool logic1 = true;
+            //bool logic2 = false;
+            //bool logic3 = logic1 / logic2; this operator not compatible for type bool
+
+            // operator %
+
+            //byte number1 = 77;
+            //byte number2 = 3;
+            //byte number3 = number1 % number2; this operator not compatible for type byte
+            //byte number3 = (byte)(number1 % number2);
+
+            //sbyte number1 = -4;
+            //sbyte number2 = -66;
+            ////sbyte number3 = number1 % number2; this operator not compatible for type sbyte
+            //sbyte number3 = (sbyte)(number1 % number2);
+
+            //short number1 = -4;
+            //short number2 = 10066;
+            //short number3 = number1 % number2; this operator not compatible for type short
+            //short number3 = (short)(number1 % number2);
+
+            //ushort number1 = 4000;
+            //ushort number2 = 6;
+            ////ushort number3 = number1 % number2; this operator not compatible for type ushort
+            //ushort number3 = (ushort)(number1 % number2);
+
+            //int number1 = 40000;
+            //int number2 = 20000;
+            //int number3 = number1 % number2;
+
+            //uint number1 = 800;
+            //uint number2 = 200;
+            //uint number3 = number1 % number2;
+
+            //long number1 = 4;
+            //long number2 = -20000;
+            //long number3 = number1 % number2;
+
+            //ulong number1 = 40000;
+            //ulong number2 = 20000;
+            //ulong number3 = number1 % number2;
+
+            //double number1 = 4;
+            //double number2 = 20000;
+            //double number3 = number1 % number2;
+
+            //float number1 = 4;
+            //float number2 = 20000;
+            //float number3 = number1 % number2;
+
+            //char symbol1 = '1';
+            //char symbol2 = '2';
+            ////char symbol3 = symbol1 % symbol2;  this operator not compatible for type char
+            //char symbol3 = (char)(symbol1 % symbol2);
+            //Console.WriteLine(symbol3); 
+            //Console.ReadKey();
+
+            //bool logic1 = true;
+            //bool logic2 = false;
+            //bool logic3 = logic1 % logic2; this operator not compatible for type bool
+
+            //унартные операторы
+
+            //byte number1 = 77;
+            //byte number2 = number1++;
+            //byte number3 = ++number1;
+            //byte number4 = number1--;
+            //byte number5 = --number1;
+
+            //sbyte number1 = 20;
+            //sbyte number2 = number1++;
+            //sbyte number3 = ++number1;
+            //sbyte number4 = number1--;
+            //sbyte number5 = --number1;
+
+            //short number1 = -4;
+            //short number2 = number1++;
+            //short number3 = ++number1;
+            //short number4 = number1--;
+            //short number5 = --number1;
+
+            //ushort number1 = 4;
+            //ushort number2 = number1++;
+            //ushort number3 = ++number1;
+            //ushort number4 = number1--;
+            //ushort number5 = --number1;
+
+            //int number1 = 40000;
+            //int number2 = number1++;
+            //int number3 = ++number1;
+            //int number4 = number1--;
+            //int number5 = --number1;
+
+            //uint number1 = 30000;
+            //uint number2 = number1++;
+            //uint number3 = ++number1;
+            //uint number4 = number1--;
+            //uint number5 = --number1;
+
+            //long number1 = 4;
+            //long number2 = number1++;
+            //long number3 = ++number1;
+            //long number4 = number1--;
+            //long number5 = --number1;
+
+
+            //ulong number1 = 40000;
+            //ulong number2 = number1++;
+            //ulong number3 = ++number1;
+            //ulong number4 = number1--;
+            //ulong number5 = --number1;
+
+            //double number1 = 4;
+            //double number2 = number1++;
+            //double number3 = ++number1;
+            //double number4 = number1--;
+            //double number5 = --number1;
+
+            //float number1 = 4;
+            //float number2 = number1++;
+            //float number3 = ++number1;
+            //float number4 = number1--;
+            //float number5 = --number1;
+
+            //char symbol1 = '1';
+            //char symbol2 = symbol1++;
+            //char symbol3 = ++symbol1;
+            //char symbol3 = symbol1--;
+            //char symbol4 = --symbol1;
+            //Console.WriteLine(symbol2);
+            //Console.ReadKey();
+
+            //bool logic1 = true;
+            //bool logic2 = logic1++; this operator not compatible for type bool
+            //bool logic2 = ++logic1; this operator not compatible for type bool
+            //bool logic4 = logic1--; this operator not compatible for type bool
+            //bool logic5 = --logic1; this operator not compatible for type bool
+
+            // тернарный оператор
+
+            //byte number1 = 6;
+            //byte number2 = 3;
+            //byte number3 = number1 < number2 ? (number1 + number2) : (number1 - number2); this operator not compatible for type byte
+            //byte number3 = (byte)(number1 < number2 ? (number1 * number2) : (number1 - number2));
+
+            //sbyte number1 = 6;
+            //sbyte number2 = 3;
+            //sbyte number3 = number1 < number2 ? (number1 + number2) : (number1 - number2); this operator not compatible for type sbyte
+            //sbyte number3 = (sbyte)(number1 < number2 ? (number1 * number2) : (number1 - number2));
+
+            //short number1 = 6;
+            //short number2 = 3;
+            //short number3 = number1 < number2 ? (number1 + number2) : (number1 - number2); this operator not compatible for type short
+            //short number3 = (short)(number1 < number2 ? (number1 * number2) : (number1 - number2));
+
+            //ushort number1 = 6;
+            //ushort number2 = 3;
+            //ushort number3 = number1 < number2 ? (number1 + number2) : (number1 - number2); this operator not compatible for type ushort
+            //ushort number3 = (short)(number1 < number2 ? (number1 * number2) : (number1 - number2));
+
+            //int number1 = 4;
+            //int number2 = 20000;
+            //int number3 = number1 < number2 ? (number1 * number2) : (number1 - number2);
+
+            //uint number1 = 4;
+            //uint number2 = 20000;
+            //uint number3 = number1 < number2 ? (number1 * number2) : (number1 + number2);
+
+            //long number1 = 4;
+            //long number2 = -20000;
+            //long number3 = number1 < number2 ? (number1 * number2) : (number1 - number2);
+
+            //ulong number1 = 4;
+            //ulong number2 = 20000;
+            //ulong number3 = number1 < number2 ? (number1 * number2) : (number1 + number2);
+
+            //double number1 = 4;
+            //double number2 = 20000;
+            //double number3 = number1 < number2 ? (number1 * number2) : (number1 - number2);
+
+            //float number1 = 4;
+            //float number2 = 20000;
+            //float number3 = number1 < number2 ? (number1 * number2) : (number1 - number2);
+
+            //char symbol1 = '1';
+            //char symbol2 = '2';
+
+            //bool v = symbol1 != symbol2;
+            //char symbol3 = v ? symbol1 :symbol2;
+            //Console.ReadKey();
+
+            //bool logic1 = true;
+            //bool logic2 = false;
+            //bool logic3 = logic1 == logic2 ? logic1 : logic2;
+        }
+    }
+}
